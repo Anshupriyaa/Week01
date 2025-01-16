be1c115a6016b289b4d90a6ac7d390222b9cefa8
Anshupriyaa <165270222+Anshupriyaa@users.noreply.github.com>
2025-01-16 13:25:04 +0530

    Update StudentGradingSystem .java

--- Day05-Level2/StudentGradingSystem .java
+++ Day05-Level2/StudentGradingSystem .java
@@ -1,10 +1,4 @@
-/*Create a program to take input marks of students in 3 subjects physics, chemistry, and maths. Compute the percentage and then calculate the grade as shown in figure below
-Hint => 
-Write a method to generate random 2-digit scores for Physics, Chemistry and Math (PCM) for the students and return the scores. This method returns a 2D array with PCM scores for all students
-Write a Method to calculate the total, average, and percentages for each student and return a 2D array with the corresponding values. Please ensure to round off the values to 2 Digits using Math.round() method
-Write a Method to calculate the grade based on the percentage as shown in the ref table and return a 2D array of students' grade
-Finally write a Method to display the scorecard of all students with their scores, total, average, percentage, and grade in a tabular format. 
-*/
+
 import java.util.Random;
 import java.util.Scanner;
 public class StudentGradingSystem {
